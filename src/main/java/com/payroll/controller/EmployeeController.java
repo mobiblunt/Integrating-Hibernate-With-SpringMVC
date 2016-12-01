9d780f791c0d175ae3d4c32469fbca5e3f497fd0
Mobi Blunt <mobiblunt@gmail.com>
2016-12-01 19:05:21 +0100

    edited commit

--- src/main/java/com/payroll/controller/EmployeeController.java
+++ src/main/java/com/payroll/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
-@RequestMapping("/employee")
+@RequestMapping({"/","/employee"})
 public class EmployeeController {
     
     @Autowired
